1a0852d0232a80762f83f86f217e1ab7f4f34f03
brunocampos01 <brunocampos01@gmail.com>
2019-07-06 14:58:30 -0300

    refactor: structure

---
+++ src/environment/jupyter_notebook_config.py
@@ -0,0 +1,20 @@
+from jupyter_core.paths import jupyter_data_dir
+import subprocess
+import os
+import stat
+
+c = get_config()
+
+c.NotebookApp.ip = '0.0.0.0'
+
+# The port the notebook server will listen on
+c.NotebookApp.port = 8888
+
+# Whether to open in a browser after starting
+c.NotebookApp.open_browser = False
+
+# Set the Access-Control-Allow-Credentials: true header
+c.NotebookApp.allow_password_change = False
+
+# https://github.com/jupyter/notebook/issues/3130
+c.FileContentsManager.delete_to_trash = False
